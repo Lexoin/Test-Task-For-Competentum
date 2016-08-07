424b43239f5106f245903b4badccaf416c9be87e
Aleksey <lex.marochckin@yandex.ru>
2016-08-07 16:45:38 +0300

    Добавлена разметка областей, добавлен класс Ball

--- script.js
+++ script.js
@@ -1,17 +1,85 @@
 
+var xCanvas = 0;
+var yCanvas = 0;
+var heightCanvas = 640;
+var widthCanvas = 1460;
+
+var xField = 20;
+var yField = 20;
+var heightField = 600;
+var widthField = 1000;
+
+var xStore = 1040;
+var yStore = 20;
+var heightStore = 600;
+var widhtStore = 400;
+
+var xMenu = 1500;
+var yMenu = 0;
+var heightMenu = 640;
+var widthMenu = 300;
+
 var X = 0;
 var Y = 0;
 
 var PX = -100;
 var PY = -100
 
+var storeBalls = [];
+var fieldBalls = [];
+
 var Canvas = document.getElementById("TestCanvas");
 var Context = Canvas.getContext('2d');
 
+
+function Ball(r, x, y, vx, vy){
+	
+	var x = x;
+	var y = y;
+	var vx = vx;
+	var vy = vy;
+	var r = r;
+	
+	this.drawBall = function(Context){
+		Context.beginPath();
+		Context.arc(x, y, r, 0, 2 * Math.PI, false);
+		Context.fill();
+	};
+}
+
+function initStore(){
+	
+	for(i=0; i<5; i++){
+		storeBalls[i] = new Ball(15, xStore + i * 20 + 20 , yStore + i * 20 + 20, 0, 0);
+	}
+}
+
+function drawCanvas(){
+	Context.strokeRect(0, 0, widthCanvas, heightCanvas);
+}
+
+function drawField(){
+	Context.strokeRect(xField, yField, widthField, heightField);
+}
+
+function drawStore(){
+	Context.strokeRect(xStore, yStore, widhtStore, heightStore);
+	
+	for(i = 0; i < 5; i++){
+		storeBalls[i].drawBall(Context);
+	}
+}
+
+function drawMenu(){
+	Context.strokeRect(xMenu, yMenu, widthMenu, heightMenu);
+}
+
 function Draw() {
-		Context.moveTo(X,Y);
-		Context.fillRect(X, Y, 20, 20);
-		Context.moveTo(0,0);
+		drawCanvas();
+		drawField();
+		drawStore();
+		drawMenu();
+		//Context.fillRect(X, Y, 20, 20);
 }
 
 function Calculate(){
@@ -30,13 +98,12 @@ function Calculate(){
 
 function Clear(){
 	//Context.clearRect(0, 0, 600, 600);
-	Context.clearRect(X, Y, 20, 20);
+	Context.clearRect(X, Y, widthCanvas, heightCanvas);
 }
-
 
 function Render(){
 	Clear();
-	Calculate();
+	//Calculate();
 	Draw();
 	
 }
@@ -54,5 +121,6 @@ function Test(event){
 	Context.moveTo(0,0);
 }
 
-document.getElementById("TestCanvas").onmousemove = Test;
-setInterval(Render,500);
+initStore();
+//document.getElementById("TestCanvas").onmousemove = Test;
+setInterval(Render,20);
