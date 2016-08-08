1376e41c17cd5193997055509ae7128fd8920d1e
Aleksey <lex.marochckin@yandex.ru>
2016-08-08 20:05:42 +0300

    Добавлено управление полетом шарика, пофикшен баг с огромной скоростью полета шарика

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 //Размеры всей канвы
 var height = 640;
-var width = 1800;
+var width = 1920;
 
 //Координаты и размеры всего полотна
 var xCanvas = 0;
@@ -34,8 +34,17 @@ var storeBalls = [];
 var countFieldBalls = 0;
 var fieldBalls = [];
 
+//Текущее положение стика управления скоростью и направлением движения шарика
+var xStick = xMenu + widthMenu / 2;
+var yStick = yMenu + heightMenu / 3;
+var stickAllocated = 0;
+var xCenterStick = xMenu + widthMenu / 2;
+var yCenterStick = yMenu + heightMenu / 3;
+
 //Перетаскиваемый шар
 var draggedBall = null;
+//Выделенный шар
+var allocatedBall = null;
 
 var Canvas = document.getElementById("TestCanvas");
 var Context = Canvas.getContext('2d');
@@ -92,6 +101,21 @@ function vector2D(x, y){
 		return result;
 	}
 	
+	this.checkMax = function(maxValue){
+		if(self.x > maxValue){
+			self.x = maxValue;
+		}
+		if(self.x < -maxValue){
+			self.x = -maxValue;
+		}
+		
+		if(self.x > maxValue){
+			self.x = maxValue;
+		}
+		if(self.x < -maxValue){
+			self.x = -maxValue;
+		}
+	}
 }
 
 function calculateCollisionWithBorders(){
@@ -123,6 +147,71 @@ function calculateCollisionWithBorders(){
 			fieldBalls[i].y = yField - rBall + heightField - CoefExtrude;
 			fieldBalls[i].vy *= -1;	
 		} 
+	}
+	
+	for(i = 0; i < countStoreBalls; i++){
+		
+		var CoefExtrude = 1;
+		var xBall = storeBalls[i].x;
+		var yBall = storeBalls[i].y;
+		var rBall = storeBalls[i].r;
+		
+		
+		if( (xBall - rBall) <= xStore ){
+			storeBalls[i].x = xStore + rBall + CoefExtrude;
+			storeBalls[i].vx *= -1;
+		} 
+		
+		if( (xBall + rBall) >= (xStore + widhtStore) ){
+			storeBalls[i].x = xStore - rBall + widhtStore - CoefExtrude;
+			storeBalls[i].vx *= -1;	
+		} 
+		
+		if( (yBall - rBall) <= yStore ){
+			storeBalls[i].y = yStore + rBall + CoefExtrude;
+			storeBalls[i].vy *= -1;
+			
+		} 
+		
+		if( (yBall + rBall) >= (yField + heightStore) ){
+			storeBalls[i].y = yField - rBall + heightStore - CoefExtrude;
+			storeBalls[i].vy *= -1;	
+		} 
+	}
+	
+}
+
+function calculateCollisionsBallsWithBallsIntoStore(){
+	for(i = 0; i < countStoreBalls; i++){
+		
+		for(j = i + 1; j < countStoreBalls; j++){	
+			var xBall1 = storeBalls[i].x;
+			var yBall1 = storeBalls[i].y;
+			var rBall1 = storeBalls[i].r;
+			var speed1 = new vector2D(storeBalls[i].vx, storeBalls[i].vy);
+			
+			var xBall2 = storeBalls[j].x;
+			var yBall2 = storeBalls[j].y;
+			var rBall2 = storeBalls[j].r;
+			var speed2 = new vector2D(storeBalls[j].vx, storeBalls[j].vy);
+			
+			var requiredDistanse = rBall1 + rBall2;
+			var currentDistanse = Math.sqrt( Math.pow((xBall1 - xBall2), 2) + Math.pow((yBall1 - yBall2), 2)); 
+			
+			if(currentDistanse <= requiredDistanse){
+				var axis2 = (new vector2D(xBall2-xBall1, yBall2-yBall1));
+				var axis1 = (new vector2D(xBall1-xBall2, yBall1-yBall2));
+	
+				axis1.normalize();
+				axis2.normalize();
+				
+				storeBalls[i].x += axis1.x * 2;
+				storeBalls[i].y += axis1.y * 2;
+				
+				storeBalls[j].x += axis2.x * 2;
+				storeBalls[j].y += axis2.y * 2;
+			}
+		}
 	}
 }
 
@@ -159,6 +248,9 @@ function calculateCollisionBallsWithBalls(){
 				vectorSpeed1 = vectorSpeed1.multiply(currentSpeed);
 				vectorSpeed2 = vectorSpeed2.multiply(currentSpeed);
 				
+				//vectorSpeed1.checkMax(10);
+				//vectorSpeed2.checkMax(10);
+				
 				fieldBalls[i].vx = vectorSpeed1.x;
 				fieldBalls[i].vy = vectorSpeed1.y;
 				
@@ -185,10 +277,21 @@ function calculatePositions(){
 	}
 }
 
+function checkMaxBallSpeed(){
+	for(i = 0; i < countFieldBalls; i++){
+		var speedVector = new vector2D(fieldBalls[i].vx, fieldBalls[i].vy);
+		speedVector.checkMax(10);
+		fieldBalls[i].vx = speedVector.x;
+		fieldBalls[i].vy = speedVector.y;
+	}
+}
+
 function calculate(){
 	calculatePositions();
 	calculateCollisionWithBorders();
 	calculateCollisionBallsWithBalls();
+	calculateCollisionsBallsWithBallsIntoStore();
+	checkMaxBallSpeed();
 }
 
 function initStore(){
@@ -224,6 +327,41 @@ function drawStore(){
 
 function drawMenu(){
 	Context.strokeRect(xMenu, yMenu, widthMenu, heightMenu);
+	
+	Context.beginPath();
+	Context.arc(xMenu + widthMenu / 2, yMenu + heightMenu / 3, 100, 0, 2 * Math.PI, false);
+	Context.stroke();
+	
+	Context.beginPath();
+	Context.arc(xMenu + widthMenu / 2, yMenu + heightMenu / 3, 5, 0, 2 * Math.PI, false);
+	Context.stroke();
+	
+	if(allocatedBall != null && stickAllocated != 1){
+		var vector = new vector2D(allocatedBall.vx, allocatedBall.vy);
+		var speed = vector.length() * 10;
+		vector.normalize();
+		vector = vector.multiply(speed);
+		xStick = vector.x + xMenu + widthMenu / 2;
+		yStick = vector.y + yMenu + heightMenu / 3;
+		Context.beginPath();
+		Context.arc(xStick, yStick, 10, 0, 2 * Math.PI, false);
+		Context.fill();
+	}
+	else{
+		Context.beginPath();
+		Context.arc(xStick, yStick, 10, 0, 2 * Math.PI, false);
+		Context.fill();
+	}
+}
+
+function drawAllocatedBall(){
+	if(allocatedBall != null){
+		Context.beginPath();
+		Context.strokeStyle = "red";
+		Context.arc(allocatedBall.x, allocatedBall.y, allocatedBall.r+5, 0, 2 * Math.PI, false);
+		Context.stroke();
+		Context.strokeStyle = "black";
+	}
 }
 
 function draw() {
@@ -231,6 +369,7 @@ function draw() {
 		drawField();
 		drawStore();
 		drawMenu();
+		drawAllocatedBall();
 }
 
 function clear(){
@@ -255,6 +394,7 @@ function mouseDownHandler(event){
 		
 		if( Math.pow((mouseX - xBall),2) + Math.pow((mouseY - yBall),2) <= Math.pow(rBall,2) ){
 			draggedBall = storeBalls[i];
+			allocatedBall = storeBalls[i];;
 			storeBalls.splice(i, 1);
 			countStoreBalls--;
 			break;
@@ -268,11 +408,17 @@ function mouseDownHandler(event){
 		
 		if( Math.pow((mouseX - xBall),2) + Math.pow((mouseY - yBall),2) <= Math.pow(rBall,2) ){
 			draggedBall = fieldBalls[i];
+			allocatedBall = fieldBalls[i];;
 			fieldBalls.splice(i, 1);
 			countFieldBalls--;
 			break;
 		}
 	}
+	
+	if( Math.pow((mouseX - xStick),2) + Math.pow((mouseY - yStick),2) <= Math.pow(10,2) ){
+		stickAllocated = 1;
+	}
+	
 }
 
 function mouseMoveHandler(event){
@@ -280,6 +426,24 @@ function mouseMoveHandler(event){
 	if(draggedBall != null){
 		draggedBall.x = event.clientX;
 		draggedBall.y = event.clientY;
+	}
+	
+	if(stickAllocated == 1){
+		if( Math.sqrt( Math.pow((event.clientX - xCenterStick), 2) + Math.pow((event.clientY - yCenterStick), 2)) < 100){
+			xStick = event.clientX;
+			yStick = event.clientY;
+			allocatedBall.vx = (xStick - xCenterStick)/10;
+			allocatedBall.vy = (yStick - yCenterStick)/10;
+		}
+		else{
+			var vectorToCenterStick = new vector2D(event.clientX - xCenterStick, event.clientY - yCenterStick);
+			vectorToCenterStick.normalize();
+			vectorToCenterStick = vectorToCenterStick.multiply(90);
+			xStick = vectorToCenterStick.x + xCenterStick;
+			yStick = vectorToCenterStick.y + yCenterStick;
+			allocatedBall.vx = (xStick - xCenterStick)/10;
+			allocatedBall.vy = (yStick - yCenterStick)/10;
+		}
 	}
 }
 
@@ -295,13 +459,26 @@ function mouseUpHandler(event){
 			  
 		  }
 		else{
-			draggedBall.x = xStore + widhtStore - 100;
-			draggedBall.y = yStore + heightStore - 100;
-			storeBalls[countStoreBalls] = draggedBall;
-			countStoreBalls++;
-			draggedBall = null;
+			if(((xBall >= xStore) && (xBall <= xStore + widhtStore)) && ((yBall >= yStore) && (yBall <= yStore + heightStore))){
+				draggedBall.vx = 1;
+				draggedBall.vy = -1;
+				storeBalls[countStoreBalls] = draggedBall;
+				countStoreBalls++;
+				draggedBall = null;
+			}
+			else{
+				
+				draggedBall.x = xStore + widhtStore/2;
+				draggedBall.y = yStore + heightStore/2;
+				storeBalls[countStoreBalls] = draggedBall;
+				countStoreBalls++;
+				draggedBall = null;
+			}	
 		}	
-		
+	}
+	
+	if(stickAllocated == 1){
+		stickAllocated = 0;
 	}
 }
 
